2a8f1e07752b351c7041867226d539182879bebf
Pär <saltomortal86@gmail.com>
2021-10-01 16:52:47 +0200

    changed to cdn

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './style.css'
 
-import * as THREE from 'three';
+import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.js';
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
